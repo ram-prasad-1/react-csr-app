e5dc7c3844314803c195da420225ee6bc6ccc3c6
ram-toteai <ram@tote.ai>
2024-10-31 00:22:44 +0530

    few changes

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,10 +3,12 @@ import Home from '@/pages/Home'
 import ClassComp from '@/pages/react/class-comp'
 import DummyPage from '@/pages/DummyPage'
 import NoMatch from '@/pages/NoMatch'
+import UseCallback from '@/pages/react/usecallback'
 
 export const router = createBrowserRouter([
   {path: '/', element: <Home />, errorElement: <NoMatch />},
   {path: '/dummy', element: <DummyPage />},
 
   {path: '/react/class-comp', element: <ClassComp />},
+  {path: '/react/useCallback', element: <UseCallback />},
 ])
